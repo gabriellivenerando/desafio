f1d3669cb69fb89b6214f50d686483df102c4d17
gabrielli <gabieven14@gmail.com>
2020-08-06 20:30:57 -0300

    botão e lógica embaralhar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,75 +19,89 @@ class Home extends React.Component {
             newListTwo: []
         }
     }
-    
+
     componentDidMount = () => {
-        this.setState({name: groups})
-        
+        this.setState({ name: groups })
+
     };
 
-  
-    
+
+
     changeGroup = (e) => {
 
         let id = e.target.id;
 
         switch (id) {
             case "1":
-                return this.setState({ group: "1", newListTwo: [], changeColorDefault:"btnGroup__action", changeColorTwo: "", changeColorThree: ""  })
+                return this.setState({ group: "1", newListTwo: [], changeColorDefault: "btnGroup__action", changeColorTwo: "", changeColorThree: "", orderGroup: "one"})
             case "2":
-                return this.setState({ group: "2", newListOne: [], changeColorDefault: "", changeColorTwo: "btnGroup__action", changeColorThree:"" })
+                return this.setState({ group: "2", newListOne: [], changeColorDefault: "", changeColorTwo: "btnGroup__action", changeColorThree: "" , orderGroup: "two"})
             case "3":
-                return this.setState({ group: "3", changeColorTwo: "", changeColorThree:"btnGroup__action",changeColorDefault:""})
+                return this.setState({ group: "3", changeColorTwo: "", changeColorThree: "btnGroup__action", changeColorDefault: "" })
             default:
                 return this.setState({ group: "Esse grupo ainda não existe no nosso sistema" })
         }
     }
 
 
-    order = () =>{
-        
-        const {name} = this.state; 
+    order = () => {
+
+        const { name } = this.state;
         let orderNameGroupOne = [];
         let orderNameGroupTwo = [];
 
         let newListOneOrder = [];
         let newListTwoOrder = [];
-        
-        
+
+
         switch (this.state.orderGroup) {
             case "one":
-                    alert("um")
-                orderNameGroupOne = name.groupOne.map( (item) => {
+                alert("um")
+                orderNameGroupOne = name.groupOne.map((item) => {
                     return item.name
                 })
-                newListOneOrder = orderNameGroupOne.sort((a,b) => a.localeCompare(b, 'pt-br'));
-                
-                return this.setState({orderGroup: "one", newListOne: newListOneOrder, group: [] })
-            
+                newListOneOrder = orderNameGroupOne.sort((a, b) => a.localeCompare(b, 'pt-br'));
+
+                return this.setState({ orderGroup: "one", newListOne: newListOneOrder, group: [] })
+
 
             case "two":
-                    alert("dois")
-                orderNameGroupTwo = name.groupTwo.map( (item) => {
+                alert("dois")
+                orderNameGroupTwo = name.groupTwo.map((item) => {
                     return item.name
                 })
-            
-                newListTwoOrder = orderNameGroupTwo.sort((a,b) => a.localeCompare(b, 'pt-br'));
-                return this.setState({orderGroup: "two", newListTwo: newListTwoOrder, group: []})
-                
+
+                newListTwoOrder = orderNameGroupTwo.sort((a, b) => a.localeCompare(b, 'pt-br'));
+                return this.setState({ orderGroup: "two", newListTwo: newListTwoOrder, group: [] })
+
             default:
                 return alert('e ai?')
         }
+
+    }
+
+
+    randomList = () =>{
         
+        const {name} = this.state;
+        let testeArray = name.groupOne.map((item) => {
+            return item.name
+        })
+        
+            for (var j, x, i = testeArray.length; i; j = Math.floor(Math.random() * i), x = testeArray[--i], testeArray[i] = testeArray[j], testeArray[j] = x);
+            
+            
+                 return console.log(testeArray)
     }
 
     render() {
-        
-        const { name, newListOne, newListTwo } = this.state;
-  
-        
-        
 
-        if(name.groupOne === undefined) return <div></div>
+        const { name, newListOne, newListTwo } = this.state;
+
+
+
+
+        if (name.groupOne === undefined) return <div></div>
 
         return (
             <section className="table">
@@ -114,16 +128,16 @@ class Home extends React.Component {
                     </div>
                     <div className="table__container--names">
                         {this.state.group === "1" && (
-                            <> 
+                            <>
                                 {name.groupOne.map(item => (
-                                <ul>
-                                    <li>{item.name}</li>
-                                </ul>
+                                    <ul>
+                                        <li>{item.name}</li>
+                                    </ul>
                                 ))}
-                                
+
                             </>
                         )}
-                        
+
                         {this.state.orderGroup === "one" && (
                             <>
                                 {newListOne.map(item => (
@@ -133,12 +147,12 @@ class Home extends React.Component {
                                 ))}
                             </>
                         )}
-                        
+
 
                         {this.state.group === "2" && (
-                        <>   
-                            {name.groupTwo.map(item => (<p>{item.name}</p>))}
-                        </>
+                            <>
+                                {name.groupTwo.map(item => (<p>{item.name}</p>))}
+                            </>
                         )}
 
                         {this.state.orderGroup === "two" && (
@@ -152,22 +166,22 @@ class Home extends React.Component {
                         )}
 
                         {this.state.group === "3" && (
-                        <>   
-                            {name.groupThree.map(item => (<p>{item.name}</p>))}
-                        </>
+                            <>
+                                {name.groupThree.map(item => (<p>{item.name}</p>))}
+                            </>
                         )}
                     </div>
-                        <div className="btnOrderTypes__container">
-                            <div className="btnOrderTypes__nameColor">
-                                <h6>ORDENAR POR:</h6>
-                                <button className="btnOrderTypes_name" onClick={this.order}>Nome</button>
-                                <button className="btnOrderTypes_color"onClick={this.order}>Cor</button>
-                            </div>
-                            <div>
-                            <button className="btnOrderTypes_random" >Embaralhar</button>
-                            </div>
-
+                    <div className="btnOrderTypes__container">
+                        <div className="btnOrderTypes__nameColor">
+                            <h6>ORDENAR POR:</h6>
+                            <button className="btnOrderTypes_name" onClick={this.order}>Nome</button>
+                            <button className="btnOrderTypes_color" onClick={this.order}>Cor</button>
                         </div>
+                        <div>
+                            <button className="btnOrderTypes_random" onClick={this.randomList} >Embaralhar</button>
+                        </div>
+
+                    </div>
                 </div>
             </section>
         )
